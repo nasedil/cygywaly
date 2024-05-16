ae91ca5c747826d9601871327056b1a4fa7331f3
Eugene Petkevich <nasedil.genio.code@gmail.com>
2024-05-16 22:33:35 +0200

    add blinking / fix vertical perspective

--- world.py
+++ world.py
@@ -4,7 +4,7 @@ import pygame
 import pygame.gfxdraw
 
 STAR_DENSITY = 1000
-STAR_RADIUS = 0.02
+STAR_RADIUS = 0.005
 HEIGHT = 1
 VIEW_WIDTH = 2
 
@@ -13,20 +13,23 @@ class World(object):
         self.length = length
         self.difficulty = difficulty
         self.back_stars = []
-        self.back_color = (0, 0, 0)
-        self.back_stars_color = (randint(50, 250), randint(50, 250), randint(50, 250), 128)
+        self.back_color = (10, 2, 15)
+        self.back_stars_color = [200 for i in range(3)]
+        self.blink_range = 50
         self.generate()
 
     def generate(self):
-        self.back_stars = [(uniform(0, self.length), uniform(0, HEIGHT), uniform(0.5, 10))
+        self.back_stars = [(uniform(0, self.length), uniform(-HEIGHT, HEIGHT), uniform(0.5, 10))
                            for i in range(int(STAR_DENSITY*self.length))]
 
     def draw(self, surface, position_x):
-        window_width, _ = pygame.display.get_window_size()
+        window_width, window_height = pygame.display.get_window_size()
         scale = window_width / VIEW_WIDTH
+        VIEW_HEIGHT = window_height / scale
         for star in self.back_stars:
             x = (star[0] - position_x) / star[2] + (VIEW_WIDTH / 2)
+            y = star[1] / star[2] + (VIEW_HEIGHT / 2)
             radius = STAR_RADIUS / star[2]
-            color = (randint(50, 250), randint(50, 250), randint(50, 250), 128)
-            #pygame.draw.circle(surface, self.back_stars_color, (x*scale, star[1]*scale), radius*scale)
-            pygame.gfxdraw.filled_circle(surface, int(x*scale), int(star[1]*scale), int(radius*scale), color)
+            color = [i + randint(0, self.blink_range) for i in self.back_stars_color]
+            pygame.draw.circle(surface, color, (x*scale, window_height-y*scale), radius*scale)
+            #pygame.gfxdraw.filled_circle(surface, int(x*scale), int(star[1]*scale), int(radius*scale), color)
